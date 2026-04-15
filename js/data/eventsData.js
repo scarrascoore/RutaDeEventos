d1a88f0e2f4cfe5054278e33dd3f6fd666c10604
scarrascoore <scarrascoore@gmail.com>
2026-04-15 16:05:31 -0500

    Agregando data

---
+++ js/data/eventsData.js
@@ -0,0 +1,11 @@
+const initialData = [
+  // PEGA AQUÍ TODO tu arreglo original initialData
+  // No cambies la estructura de los objetos.
+];
+
+const days = [
+  { id: "2026-04-16", label: "Día 1", dateStr: "16 de Abr" },
+  { id: "2026-04-17", label: "Día 2", dateStr: "17 de Abr" },
+  { id: "2026-04-18", label: "Día 3", dateStr: "18 de Abr" },
+  { id: "2026-04-19", label: "Día 4", dateStr: "19 de Abr" },
+];
